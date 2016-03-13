3a26f79ce544365f86ba05c6d67ca90ccd7ca468
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-03-13 23:17:55 +0100

    implement IExchangeRatesProvider.GetExchangeRatesHistory in NBP provider + NbpExchangeRatesProvider general refactoring + NbpExchangeRatesProvider exception handling

--- KursyWalut/Provider/NbpExchangeRatesProvider.cs
+++ KursyWalut/Provider/NbpExchangeRatesProvider.cs
@@ -14,6 +14,8 @@ namespace KursyWalut.Provider
 {
     internal class NbpExchangeRatesProvider : IExchangeRatesProvider
     {
+        private readonly Encoding _utf8;
+        private readonly Encoding _iso88592;
         private readonly Semaphore _lock = new Semaphore(1, 1);
 
         private readonly IList<IObserver<int>> _observers = new List<IObserver<int>>();
@@ -22,99 +24,291 @@ namespace KursyWalut.Provider
         public NbpExchangeRatesProvider()
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            _utf8 = Encoding.UTF8;
+            _iso88592 = Encoding.GetEncoding("ISO-8859-2");
         }
 
+        /// <exception cref="T:System.IO.IOException">Response unavailable or in unexpected format.</exception>
         public async Task<IList<DateTime>> GetAvailableDates()
         {
-            _lock.WaitOne();
-            NotifyObservers(0);
-            var result = new List<DateTime>();
-
-            string[] filenames;
-            using (var client = new HttpClient())
-            {
-                var dirResult = await client.GetStringAsync("http://www.nbp.pl/kursy/xml/dir.txt");
-                filenames = dirResult.Split('\n');
-            }
-
-            NotifyObservers(70);
-            foreach (var filename in filenames)
-            {
-                if (!filename.StartsWith("a"))
-                    continue;
-
-                var day = FilenameToDateTime(filename);
-                _dateToFilename.Add(day, filename);
-                result.Add(day);
-            }
-
-            NotifyObservers(100);
-            _lock.Release();
-            return result.AsReadOnly();
+            return await GetAvailableDates2(ProgressNotify.Master);
         }
 
-
+        /// <exception cref="T:System.ArgumentException">Day was not returned by GetAvailableDates().</exception>
+        /// <exception cref="T:System.IO.IOException">Response unavailable or in unexpected format.</exception>
         public async Task<IList<ExchangeRate>> GetExchangeRates(DateTime day)
         {
-            _lock.WaitOne();
-            NotifyObservers(0);
+            return await GetExchangeRates2(day, ProgressNotify.Master);
+        }
 
-            if (!_dateToFilename.ContainsKey(day))
-                throw new ArgumentException();
-            var filename = _dateToFilename[day];
-
-            XDocument xmlDoc;
-            using (var client = new HttpClient())
-            {
-                var xmlBytes = await client.GetByteArrayAsync("http://www.nbp.pl/kursy/xml/" + filename + ".xml");
-                var xmlString = Encoding.GetEncoding("ISO-8859-2").GetString(xmlBytes);
-                xmlDoc = XDocument.Load(new StringReader(xmlString));
-            }
-
-            NotifyObservers(70);
-            var exchangeRates = xmlDoc
-                .Descendants("tabela_kursow")
-                .Descendants("pozycja")
-                .Select(x => XmlPositionToExchangeRate(day, x));
-
-            NotifyObservers(100);
-            _lock.Release();
-            return exchangeRates.ToImmutableList();
+        /// <exception cref="T:System.ArgumentException">????</exception>
+        /// <exception cref="T:System.IO.IOException">Response unavailable or in unexpected format.</exception>
+        public async Task<IList<ExchangeRate>> GetExchangeRatesHistory(
+            Currency currency, DateTime startDay, DateTime stopDay)
+        {
+            return await GetExchangeRatesHistory2(currency, startDay, stopDay, ProgressNotify.Master);
         }
 
         public IDisposable Subscribe(IObserver<int> observer)
         {
             _observers.Add(observer);
-            return null;
+            return new ObserverDisposable(this, observer);
         }
 
-        private DateTime FilenameToDateTime(string fileName)
+        /// internal version of GetAvailableDate; customizable lock, and progress start/stop value
+        private async Task<IList<DateTime>> GetAvailableDates2(ProgressNotify p)
         {
-            return DateTime.ParseExact(fileName.Substring(5, 6), "yyMMdd", CultureInfo.InvariantCulture);
+            if (p.IsMaster) _lock.WaitOne();
+
+            try
+            {
+                NotifyObservers(p.Start);
+
+                var dir = await GetHttpResponse("http://www.nbp.pl/kursy/xml/dir.txt", _utf8);
+                var filenames = ParseFilenames(dir);
+
+                NotifyObservers(CalculateProgress(p, 70));
+                _dateToFilename.Clear();
+
+                var result = new List<DateTime>();
+                foreach (var filename in filenames.Where(f => f.StartsWith("a")))
+                {
+                    var day = ParseDateTime(filename);
+                    _dateToFilename.Add(day, filename);
+                    result.Add(day);
+                }
+
+                NotifyObservers(p.Stop);
+                return result.AsReadOnly();
+            }
+
+            catch (Exception ex)
+            {
+                if (p.IsMaster) NotifyObservers(-1);
+                throw new IOException("response unavailable or in unexpected format", ex);
+            }
+            finally
+            {
+                if (p.IsMaster) _lock.Release();
+            }
         }
 
-        private ExchangeRate XmlPositionToExchangeRate(DateTime day, XContainer x)
+        /// internal version of GetExchangeRates; customizable lock, and progress start/stop value
+        private async Task<IList<ExchangeRate>> GetExchangeRates2(DateTime day, ProgressNotify p)
         {
-            return new ExchangeRate(
-                day,
-                XmlPositionToCurrency(x),
-                double.Parse(x.Element("kurs_sredni").Value.Replace(",", ".")));
+            if (p.IsMaster) _lock.WaitOne();
+
+            try
+            {
+                NotifyObservers(p.Start);
+                var filename = _dateToFilename[day];
+
+                var response = await GetHttpResponse("http://www.nbp.pl/kursy/xml/" + filename + ".xml", _iso88592);
+                var xml = XDocument.Load(new StringReader(response));
+                NotifyObservers(CalculateProgress(p, 70));
+
+                var exchangeRates = ParseExchangeRates(xml, day);
+                NotifyObservers(p.Stop);
+
+                return exchangeRates;
+            }
+
+            catch (Exception ex) when (ex is KeyNotFoundException)
+            {
+                if (p.IsMaster) NotifyObservers(-1);
+                throw new ArgumentException("day was not returned by GetAvailableDates()");
+            }
+            catch (Exception ex)
+            {
+                if (p.IsMaster) NotifyObservers(-1);
+                throw new IOException("response unavailable or in unexpected format", ex);
+            }
+            finally
+            {
+                if (p.IsMaster) _lock.Release();
+            }
         }
 
-        private Currency XmlPositionToCurrency(XContainer x)
+        /// internal version of GetExchangeRatesHistory; customizable lock, and progress start/stop value
+        private async Task<IList<ExchangeRate>> GetExchangeRatesHistory2(
+            Currency currency, DateTime startDay, DateTime stopDay, ProgressNotify p)
         {
-            return new Currency(
-                x.Element("kod_waluty").Value,
-                x.Element("nazwa_waluty").Value,
-                int.Parse(x.Element("przelicznik").Value));
+            if (p.IsMaster) _lock.WaitOne();
+
+            try
+            {
+                NotifyObservers(p.Start);
+
+                var days = _dateToFilename
+                    .Where(s => s.Key >= startDay && s.Key <= stopDay)
+                    .Select(s => s.Key)
+                    .OrderBy(day => day);
+
+                var progressPerDay = (p.Stop + p.Start)/days.Count();
+                var progress = p.Start;
+
+                var result = new List<ExchangeRate>();
+                foreach (var dateTime in days)
+                {
+                    var exchangeRates =
+                        await GetExchangeRates2(dateTime, new ProgressNotify(progress, progress + progressPerDay));
+                    var exchangeRate = exchangeRates.First(e => e.Currency.Equals(currency));
+                    result.Add(exchangeRate);
+                    progress += progressPerDay;
+                    NotifyObservers(progress);
+                }
+
+                return result.AsReadOnly();
+            }
+
+            catch (Exception ex)
+            {
+                if (p.IsMaster) NotifyObservers(-1);
+                throw new IOException("response unavailable or in unexpected format", ex);
+            }
+            finally
+            {
+                if (p.IsMaster) _lock.Release();
+            }
+        }
+
+        /// <exception cref="T:System.Exception">If something go wrong.</exception>
+        private async Task<string> GetHttpResponse(string requestUri, Encoding encoding)
+        {
+            using (var client = new HttpClient())
+            {
+                var bytes = await client.GetByteArrayAsync(requestUri);
+                var str = encoding.GetString(bytes);
+                return str;
+            }
+        }
+
+        /// <exception cref="T:System.FormatException">httpResponse is in invalid format.</exception>
+        private IEnumerable<string> ParseFilenames(string httpResponse)
+        {
+            var filenames = httpResponse.Split(new[] {'\n'}, StringSplitOptions.RemoveEmptyEntries);
+
+            if (filenames.Length == 0)
+                throw new FormatException("empty response");
+
+            return filenames;
+        }
+
+        /// <exception cref="T:System.FormatException">filename is in invalid format.</exception>
+        private DateTime ParseDateTime(string filename)
+        {
+            try
+            {
+                return DateTime.ParseExact(filename.Substring(5, 6), "yyMMdd", CultureInfo.InvariantCulture);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                throw new FormatException("filename is too short", ex);
+            }
+        }
+
+        /// <exception cref="T:System.FormatException">xContainer is in invalid format.</exception>
+        private IList<ExchangeRate> ParseExchangeRates(XContainer xml, DateTime day)
+        {
+            var exchangeRates = xml
+                .Descendants("tabela_kursow")
+                .Descendants("pozycja")
+                .Select(x => ParseExchangeRate(x, day));
+
+            var exchangeRatesList = exchangeRates.ToImmutableList();
+            if (exchangeRatesList.Count == 0)
+                throw new FormatException("empty response");
+
+            return exchangeRatesList;
+        }
+
+        /// <exception cref="T:System.FormatException">xContainer is in invalid format.</exception>
+        private ExchangeRate ParseExchangeRate(XContainer x, DateTime day)
+        {
+            try
+            {
+                return new ExchangeRate(
+                    day,
+                    ParseCurrency(x),
+                    double.Parse(x.Element("kurs_sredni").Value.Replace(",", ".")));
+            }
+            catch (Exception ex)
+                when (ex is NullReferenceException || ex is OverflowException || ex is ArgumentException)
+            {
+                throw new FormatException("xContainer is in invalid format");
+            }
+        }
+
+        /// <exception cref="T:System.FormatException">xContainer is in invalid format.</exception>
+        private Currency ParseCurrency(XContainer x)
+        {
+            try
+            {
+                return new Currency(
+                    x.Element("kod_waluty").Value,
+                    x.Element("nazwa_waluty").Value,
+                    int.Parse(x.Element("przelicznik").Value));
+            }
+            catch (Exception ex) when (ex is NullReferenceException || ex is OverflowException)
+            {
+                throw new FormatException("xContainer is in invalid format");
+            }
+        }
+
+        private int CalculateProgress(ProgressNotify p, int percentDone)
+        {
+            return p.Start + (p.Stop - p.Start)*(percentDone/100);
         }
 
         private void NotifyObservers(int percentDone)
         {
             foreach (var observer in _observers)
             {
-                if (percentDone == 100) observer.OnNext(percentDone);
-                else observer.OnCompleted();
+                switch (percentDone)
+                {
+                    case -1:
+                        observer.OnError(null);
+                        break;
+                    case 100:
+                        observer.OnCompleted();
+                        break;
+                    default:
+                        observer.OnNext(percentDone);
+                        break;
+                }
+            }
+        }
+
+        private class ProgressNotify
+        {
+            public static readonly ProgressNotify Master = new ProgressNotify(0, 100);
+
+            public readonly bool IsMaster;
+            public readonly int Start;
+            public readonly int Stop;
+
+            public ProgressNotify(int start, int stop)
+            {
+                IsMaster = (start == 0) && (stop == 100);
+                Start = start;
+                Stop = stop;
+            }
+        }
+
+        private class ObserverDisposable : IDisposable
+        {
+            private readonly NbpExchangeRatesProvider _parent;
+            private readonly IObserver<int> _observer;
+
+            public ObserverDisposable(NbpExchangeRatesProvider parent, IObserver<int> observer)
+            {
+                _parent = parent;
+                _observer = observer;
+            }
+
+            public void Dispose()
+            {
+                _parent._observers.Remove(_observer);
             }
         }
     }
