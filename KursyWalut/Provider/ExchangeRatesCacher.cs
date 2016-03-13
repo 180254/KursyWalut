e45df1918d1f92ff0b6bcaceac97c1d517d7b6b6
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-03-13 22:45:18 +0100

    implement IExchangeRatesProvider.GetExchangeRatesHistory in cache

--- KursyWalut/Provider/ExchangeRatesCacher.cs
+++ KursyWalut/Provider/ExchangeRatesCacher.cs
@@ -15,6 +15,9 @@ namespace KursyWalut.Provider
         private readonly IDictionary<DateTime, IList<ExchangeRate>> _dailyExchangeRates
             = new Dictionary<DateTime, IList<ExchangeRate>>();
 
+        private readonly IDictionary<Tuple<Currency, DateTime, DateTime>, IList<ExchangeRate>> _exchangeRatesHistory
+            = new Dictionary<Tuple<Currency, DateTime, DateTime>, IList<ExchangeRate>>();
+
         public ExchangeRatesCacher(IExchangeRatesProvider exchangeRatesProvider)
         {
             _exchangeRatesProvider = exchangeRatesProvider;
@@ -24,25 +27,57 @@ namespace KursyWalut.Provider
         {
             _lock.WaitOne();
 
-            if (_availableDates == null)
-                _availableDates = await _exchangeRatesProvider.GetAvailableDates();
-
-            _lock.Release();
-            return _availableDates;
+            try
+            {
+                return _availableDates ?? (_availableDates = await _exchangeRatesProvider.GetAvailableDates());
+            }
+            finally
+            {
+                _lock.Release();
+            }
         }
 
         public async Task<IList<ExchangeRate>> GetExchangeRates(DateTime day)
         {
             _lock.WaitOne();
 
-            if (_dailyExchangeRates.ContainsKey(day))
-                return _dailyExchangeRates[day];
+            try
+            {
+                if (_dailyExchangeRates.ContainsKey(day))
+                    return _dailyExchangeRates[day];
 
-            var exchangeRates = await _exchangeRatesProvider.GetExchangeRates(day);
-            _dailyExchangeRates.Add(day, exchangeRates);
+                var exchangeRates = await _exchangeRatesProvider.GetExchangeRates(day);
+                _dailyExchangeRates.Add(day, exchangeRates);
 
-            _lock.Release();
-            return exchangeRates;
+                return exchangeRates;
+            }
+            finally
+            {
+                _lock.Release();
+            }
+        }
+
+        public async Task<IList<ExchangeRate>> GetExchangeRatesHistory(
+            Currency currency, DateTime startDay, DateTime stopDay)
+        {
+            _lock.WaitOne();
+
+            try
+            {
+                var tuple = Tuple.Create(currency, startDay, stopDay);
+
+                if (_exchangeRatesHistory.ContainsKey(tuple))
+                    return _exchangeRatesHistory[tuple];
+
+                var exchangeRates = await _exchangeRatesProvider.GetExchangeRatesHistory(currency, startDay, stopDay);
+                _exchangeRatesHistory.Add(tuple, exchangeRates);
+
+                return exchangeRates;
+            }
+            finally
+            {
+                _lock.Release();
+            }
         }
 
         public IDisposable Subscribe(IObserver<int> observer)
