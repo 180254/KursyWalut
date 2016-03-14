3252248f446de0b7cc4e4f20867c385cdbe9a98b
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-03-14 02:08:41 +0100

    IExchangeRatesProvider - exceptions documented

--- KursyWalut/Provider/IExchangeRatesProvider.cs
+++ KursyWalut/Provider/IExchangeRatesProvider.cs
@@ -6,12 +6,26 @@ namespace KursyWalut.Provider
 {
     internal interface IExchangeRatesProvider : IObservable<int>
     {
+        /// <exception cref="T:System.IO.IOException">Something go wrong with I/O.</exception>
         Task<IList<int>> GetAvailableYears();
+
+        /// <exception cref="T:System.IO.IOException">Something go wrong with I/O.</exception>
         Task<IList<DateTime>> GetAvailableDates(int year);
 
+        /// <exception cref="T:System.ArgumentException">Day was not returned by GetAvailableDates(int).</exception>
+        /// <exception cref="T:System.IO.IOException">Something go wrong with I/O.</exception>
         Task<IList<ExchangeRate>> GetExchangeRates(DateTime day);
+
+        /// <exception cref="T:System.ArgumentException">Invalid currency.</exception>
+        /// <exception cref="T:System.ArgumentException">Day was not returned by GetAvailableDates(int).</exception>
+        /// <exception cref="T:System.IO.IOException">Something go wrong with I/O.</exception>
         Task<ExchangeRate> GetExchangeRate(Currency currency, DateTime day);
 
+        /// <exception cref="T:System.ArgumentException">Invalid currency.</exception>
+        /// <exception cref="T:System.ArgumentException">Start year was not returned by GetAvailableYears().</exception>
+        /// <exception cref="T:System.ArgumentException">End year was not returned by GetAvailableYears().</exception>
+        /// <exception cref="T:System.ArgumentException">Start day > stop day.</exception>
+        /// <exception cref="T:System.IO.IOException">Something go wrong with I/O.</exception>
         Task<IList<ExchangeRate>> GetExchangeRateHistory(Currency currency, DateTime startDay, DateTime stopDay);
     }
 }
