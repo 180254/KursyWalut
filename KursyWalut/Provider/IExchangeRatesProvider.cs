01742e17eeed5eb63d5bef5f77db80e73fbeda08
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-03-14 02:00:29 +0100

    modify IExchangeRatesProvider to provide support for ExchangeRates from previous years

--- KursyWalut/Provider/IExchangeRatesProvider.cs
+++ KursyWalut/Provider/IExchangeRatesProvider.cs
@@ -6,8 +6,12 @@ namespace KursyWalut.Provider
 {
     internal interface IExchangeRatesProvider : IObservable<int>
     {
-        Task<IList<DateTime>> GetAvailableDates();
+    	Task<IList<int>> GetAvailableYears();
+        Task<IList<DateTime>> GetAvailableDates(int year);
+
         Task<IList<ExchangeRate>> GetExchangeRates(DateTime day);
-        Task<IList<ExchangeRate>> GetExchangeRatesHistory(Currency currency, DateTime startDay, DateTime stopDay);
+        Task<ExchangeRate> GetExchangeRate(Currency currency, DateTime day);
+        
+        Task<IList<ExchangeRate>> GetExchangeRateHistory(Currency currency, DateTime startDay, DateTime stopDay);
     }
 }
