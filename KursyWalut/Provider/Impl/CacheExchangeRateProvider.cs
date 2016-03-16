a4178ef99da0a7c40ced54f4f557c51ec93f2ef3
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-03-16 22:52:18 +0100

    IExchangeRateProvider/Service: CacheProvider and StandardService(generic) impl with decorator pattern

---
+++ KursyWalut/Provider/Impl/CacheExchangeRateProvider.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using KursyWalut.Cache;
+
+namespace KursyWalut.Provider.Impl
+{
+    internal class CacheExchangeRateProvider : IExchangeRatesProvider
+    {
+        private readonly IExchangeRatesProvider _exchangeRatesProvider;
+        private readonly ICache _cache;
+        private readonly IDictionary<int, IList<DateTime>> _yearToDays;
+        private readonly IDictionary<DateTime, IList<ExchangeRate>> _dayToEr;
+
+        public CacheExchangeRateProvider(IExchangeRatesProvider exchangeRatesProvider, ICache cache)
+        {
+            _exchangeRatesProvider = exchangeRatesProvider;
+            _cache = cache;
+            _yearToDays = cache.GetOrSet("_yearToDays", new Dictionary<int, IList<DateTime>>());
+            _dayToEr = cache.GetOrSet("_dayToEr", new Dictionary<DateTime, IList<ExchangeRate>>());
+        }
+
+        public IDisposable Subscribe(IObserver<int> observer)
+        {
+            return _exchangeRatesProvider.Subscribe(observer);
+        }
+
+        public async Task<IList<int>> GetAvailableYears(Progress p)
+        {
+            return await Task.Run(() =>
+                _cache.GetOrCompute("_availYears", () =>
+                {
+                    var availYearsTask = _exchangeRatesProvider.GetAvailableYears(p);
+                    availYearsTask.Wait();
+                    return availYearsTask.Result;
+                }));
+        }
+
+        public async Task<IList<DateTime>> GetAvailableDays(int year, Progress p)
+        {
+            return await GetOrCalculate(_yearToDays, year,
+                () => _exchangeRatesProvider.GetAvailableDays(year, p));
+        }
+
+        public async Task<IList<ExchangeRate>> GetExchangeRates(DateTime day, Progress p)
+        {
+            return await GetOrCalculate(_dayToEr, day,
+                () => _exchangeRatesProvider.GetExchangeRates(day, p));
+        }
+
+        private async Task<TV> GetOrCalculate<TK, TV>(IDictionary<TK, TV> dict, TK key, Func<Task<TV>> supplier)
+        {
+            if (dict.ContainsKey(key))
+                return dict[key];
+
+            var value = await supplier.Invoke();
+            dict.Add(key, value);
+
+            return value;
+        }
+    }
+}
---
+++ KursyWalut/Provider/Impl/StandardExchangeRateService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KursyWalut.Provider.Impl
+{
+    internal class StandardExchangeRateService : IExchangeRatesService
+    {
+        private readonly IExchangeRatesProvider _exchangeRatesProvider;
+
+        public StandardExchangeRateService(IExchangeRatesProvider exchangeRatesProvider)
+        {
+            _exchangeRatesProvider = exchangeRatesProvider;
+        }
+
+        public IDisposable Subscribe(IObserver<int> observer)
+        {
+            return _exchangeRatesProvider.Subscribe(observer);
+        }
+
+        public async Task<IList<int>> GetAvailableYears(Progress p)
+        {
+            return await _exchangeRatesProvider.GetAvailableYears(p);
+        }
+
+        public async Task<IList<DateTime>> GetAvailableDays(int year, Progress p)
+        {
+            return await _exchangeRatesProvider.GetAvailableDays(year, p);
+        }
+
+        public async Task<IList<ExchangeRate>> GetExchangeRates(DateTime day, Progress p)
+        {
+            return await _exchangeRatesProvider.GetExchangeRates(day, p);
+        }
+
+        public async Task<ExchangeRate> GetExchangeRate(Currency currency, DateTime day, Progress p)
+        {
+            try
+            {
+                var exchangeRates = await GetExchangeRates(day, p);
+                return exchangeRates.First(e => e.Currency.Equals(currency)); // possible InvalidOperationException
+            }
+            catch (Exception ex) when (ex is InvalidOperationException)
+            {
+                throw new ArgumentException("invalid currency");
+            }
+        }
+
+        public async Task<DateTime> GetFirstAvailableDay(Progress p)
+        {
+            var firstYear = (await GetAvailableYears(p.Partial(0, 0.1))).First();
+            var firstDate = (await GetAvailableDays(firstYear, p.Partial(0.11, 1))).First();
+            return firstDate;
+        }
+
+        public async Task<DateTime> GetLastAvailableDay(Progress p)
+        {
+            var lastYear = (await GetAvailableYears(p.Partial(0, 0.1))).Last();
+            var lastDate = (await GetAvailableDays(lastYear, p.Partial(0.11, 1))).Last();
+            return lastDate;
+        }
+
+        public async Task<IList<ExchangeRate>> GetExchangeRateHistory(
+            Currency currency, DateTime startDay, DateTime stopDay, Progress p)
+        {
+            var startYear = startDay.Year;
+            var stopYear = stopDay.Year;
+
+            var availableDays = new List<DateTime>();
+            var years = Enumerable.Range(startYear, stopYear - startYear + 1).ToImmutableList();
+            var progress = p.Partial(0, 0.4);
+            for (var i = 0; i < years.Count; i++)
+            {
+                var t = years[i];
+                var progress2 = progress.Partial2(i, years.Count);
+                availableDays.AddRange(await GetAvailableDays(t, progress2));
+            }
+
+            var exchangeRates = new List<ExchangeRate>();
+            var days = availableDays.Where(day => (day >= startDay) && (day <= stopDay)).ToImmutableList();
+            progress = p.Partial(0.41, 1);
+            for (var i = 0; i < days.Count; i++)
+            {
+                var day = days[i];
+                var progress2 = progress.Partial2(i, days.Count);
+                exchangeRates.Add(await GetExchangeRate(currency, day, progress2));
+            }
+            return exchangeRates;
+        }
+    }
+}
