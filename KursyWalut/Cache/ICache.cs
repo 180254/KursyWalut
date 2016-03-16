cbd309d49b54a96a9842ce32a437f08cae61c49d
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-03-16 22:49:19 +0100

    new ICache interface, and its impl using LocalSettings in UWP

---
+++ KursyWalut/Cache/ICache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KursyWalut.Cache
+{
+    internal interface ICache
+    {
+        void Set<TR>(string key, TR value);
+
+        /// <exception cref="T:SystemInvalidCastException">Value for <paramref name="key" /> is not of type TR.</exception>
+        /// <exception cref="T:System.Collections.Generic.KeyNotFoundException">Value for <paramref name="key" /> not found.</exception>
+        TR Get<TR>(string key);
+
+        /// <exception cref="T:SystemInvalidCastException">Value for <paramref name="key" /> is not of type TR.</exception>
+        TR GetOrSet<TR>(string key, TR value);
+
+        /// <exception cref="T:SystemInvalidCastException">Value for <paramref name="key" /> is not of type TR.</exception>
+        TR GetOrCompute<TR>(string key, Func<TR> valueFunc);
+    }
+}
---
+++ KursyWalut/Cache/LocalSettingsCache.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Storage;
+
+namespace KursyWalut.Cache
+{
+    internal class LocalSettingsCache : ICache
+    {
+        private readonly ApplicationDataContainer _localSettings = ApplicationData.Current.LocalSettings;
+
+        public void Set<TR>(string key, TR value)
+        {
+            _localSettings.Values[key] = value;
+        }
+
+        public TR Get<TR>(string key)
+        {
+            return (TR) _localSettings.Values[key];
+        }
+
+        public TR GetOrSet<TR>(string key, TR value)
+        {
+            return GetOrCompute(key, () => value);
+        }
+
+        public TR GetOrCompute<TR>(string key, Func<TR> valueFunc)
+        {
+            try
+            {
+                return Get<TR>(key);
+            }
+            catch (Exception ex) when (ex is KeyNotFoundException)
+            {
+                Set(key, valueFunc.Invoke());
+                return Get<TR>(key);
+            }
+        }
+    }
+}
