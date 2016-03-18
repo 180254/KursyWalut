45e08d7c50e1723056d2be2730e1407ca5e4501d
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-03-18 22:59:58 +0100

    new idea for ICache

--- KursyWalut/Cache/ICache.cs
+++ KursyWalut/Cache/ICache.cs
@@ -4,20 +4,13 @@ namespace KursyWalut.Cache
 {
     internal interface ICache
     {
-        void Set<TR>(string key, TR value);
+        /// <exception cref="T:System.InvalidCastException">Value for <paramref name="key" /> is not of type T.</exception>
+        T Get<T>(string key, Func<T> default_);
 
-        /// <exception cref="T:System.InvalidCastException">Value for <paramref name="key" /> is not of type TR.</exception>
-        TR SetIfAbsent<TR>(string key, TR value);
-
-        TR Compute<TR>(string key, Func<TR> valueFunc);
-
-        /// <exception cref="T:System.InvalidCastException">Value for <paramref name="key" /> is not of type TR.</exception>
-        TR ComputeIfAbsent<TR>(string key, Func<TR> valueFunc);
-
-        /// <exception cref="T:System.InvalidCastException">Value for <paramref name="key" /> is not of type TR.</exception>
-        /// <exception cref="T:System.Collections.Generic.KeyNotFoundException">Value for <paramref name="key" /> not found.</exception>
-        TR Get<TR>(string key);
+        void Store<T>(string key, T value);
 
         bool Remove(string key);
+
+        void Clear();
     }
 }
---
+++ KursyWalut/Cache/InMemCache.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KursyWalut.Cache
+{
+    internal class InMemCache : ICache
+    {
+        private readonly IDictionary<string, object> _dict = new Dictionary<string, object>();
+
+        public T Get<T>(string key, Func<T> default_)
+        {
+            lock (_dict)
+            {
+                object value;
+                if (_dict.TryGetValue(key, out value))
+                {
+                    return (T) value;
+                }
+                return default_.Invoke();
+            }
+        }
+
+        public void Store<T>(string key, T value)
+        {
+            lock (_dict)
+            {
+                Remove(key);
+                _dict.Add(key, value);
+            }
+        }
+
+        public bool Remove(string key)
+        {
+            lock (_dict)
+            {
+                return _dict.Remove(key);
+            }
+        }
+
+        public void Clear()
+        {
+            lock (_dict)
+            {
+                _dict.Clear();
+            }
+        }
+    }
+}
