0fd3c11ea94990e16690fbe3b9f40b10a0839dba
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-03-28 11:18:23 +0200

    MainPage refactoring - extracted ProviderHelper - moved logic from ViewModel to .xaml.cs

--- KursyWalut/MainPage.xaml.cs
+++ KursyWalut/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.UI.Xaml.Controls;
 using GalaSoft.MvvmLight.Ioc;
+using KursyWalut.ProviderImpl;
 using KursyWalut.ViewModel;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -12,11 +14,42 @@ namespace KursyWalut
     public sealed partial class MainPage : Page
     {
         private readonly MainViewModel _mainViewModel;
+        private readonly EventHandler<int> _toProgressNotifier;
 
         public MainPage()
         {
             InitializeComponent();
             _mainViewModel = SimpleIoc.Default.GetInstance<MainViewModel>();
+            _toProgressNotifier = (sender, i) => _mainViewModel.ProgressValue.Set(i);
+
+            Init();
+        }
+
+        private async void Init()
+        {
+            using (var h = new ProviderHelper(_toProgressNotifier))
+            {
+                var lastAvailableDay =
+                    await h.ErService.GetLastAvailableDay(h.Progress.SubPercent(0.00, 0.10));
+                CalendarDatePicker.Date = lastAvailableDay;
+
+                _mainViewModel.ExchangeRates =
+                    await h.ErService.GetExchangeRates(lastAvailableDay, h.Progress.SubPercent(0.10, 0.50));
+
+                _mainViewModel.AvailDates =
+                    await h.ErService.GetAllAvailablesDay(h.Progress.SubPercent(0.50, 1.00));
+                CalendarDatePicker.IsEnabled = true;
+            }
+        }
+
+
+        private async void Reload(DateTime date)
+        {
+            using (var h = new ProviderHelper(_toProgressNotifier))
+            {
+                _mainViewModel.ExchangeRates =
+                    await h.ErService.GetExchangeRates(date, h.Progress.SubPercent(0.00, 1.00));
+            }
         }
 
         private void CalendarDatePicker_OnCalendarViewDayItemChanging(
@@ -24,17 +57,16 @@ namespace KursyWalut
             CalendarViewDayItemChangingEventArgs e)
         {
             if (e.Item != null && !_mainViewModel.AvailDates.Contains(e.Item.Date.DateTime.Date))
-            {
                 e.Item.IsBlackout = true;
-            }
         }
 
         private void CalendarDatePicker_OnDateChanged(
             CalendarDatePicker sender,
             CalendarDatePickerDateChangedEventArgs args)
         {
-            if (args.NewDate != null) { 
-                _mainViewModel.Reload(args.NewDate.Value.Date);
+            if (args.NewDate != null)
+            {
+                Reload(args.NewDate.Value.Date);
             }
         }
     }
